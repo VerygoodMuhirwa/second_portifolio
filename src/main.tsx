4e39a02b457fd5a5cd5fc3c26edd8d18673d5326
Guilherme2014-ai <guilhermehenrique78383@gmail.com>
2022-07-06 12:23:15 -0300

    init

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+// Components
+import App from './App'
+
+// CSS
+import './style/global.scss'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
