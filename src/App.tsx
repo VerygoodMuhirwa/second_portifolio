e9496a9fa74c5bd5a680db9feb5d11f32c7b8e15
Guilherme2014-ai <guilhermehenrique78383@gmail.com>
2022-07-07 15:44:25 -0300

    code lint done

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import GLogoComponent from './components/GLogoComponent'
+import React from "react";
+import GLogoComponent from "./components/GLogoComponent";
 
 // Assets
-import images from "./images.json"
-import { HomePage } from './pages/HomePage'
+import images from "./images.json";
+import { HomePage } from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App">
       <HomePage />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
